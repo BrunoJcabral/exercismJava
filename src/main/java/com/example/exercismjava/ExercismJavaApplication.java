1214702996b0ca1d4c92d761515c254db65f1fc5
OPTIMUS\bjcab <bjcabral@outlook.com>
2023-09-13 22:07:33 +0100

    commit inicial

---
+++ src/main/java/com/example/exercismjava/ExercismJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercismjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercismJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercismJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exercismjava/ExercismJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercismjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercismJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
